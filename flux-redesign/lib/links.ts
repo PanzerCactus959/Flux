926c47fa6210a7fc8a79498788e1a1371a47b914
DoQuocThai <42398238+PanzerCactus959@users.noreply.github.com>
2026-06-18 22:14:21 +0700

    Delete flux-redesign directory

--- flux-redesign/lib/links.ts
+++
@@ -1,9 +0,0 @@
-// Social + external links. TODO: thay X và Discord bằng link thật của bạn.
-export const LINKS = {
-  app: "/app",
-  x: "https://x.com/your_handle", // TODO: đổi thành tài khoản X thật
-  discord: "https://discord.gg/your_invite", // TODO: đổi thành link mời Discord thật
-  github: "https://github.com/PanzerCactus959/Flux",
-  faucet: "https://faucet.circle.com",
-  explorer: "https://testnet.arcscan.app",
-};
